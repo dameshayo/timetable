42f22d00d2a9a489e03a3efa2bf8b4809b9508ec
fredrick brighton <fredrickbrighton@gmail.com>
2023-11-20 13:27:21 +0300

    update routes

--- service/route.js
+++ service/route.js
@@ -114,7 +114,7 @@ router.get("/:type/:programme",(req,res)=>{
        
 });
 
-router.get("semesters/:year",(req,res)=>{
+router.get("/semesters/:year",(req,res)=>{
     year=req.params.year;
     try {
         axios.get(`https://ratiba.udom.ac.tz/index.php/downloads/fetch-semesters?_csrf-backend=zB-cS_yxG2p4CUZwu1IvGMzP4Efn1rOqXtr2TAOdI1a5TKQJjMVXIj9-D0D8NFh8lY6hGLGAi-Jtl8UJcPtFDA%3D%3D&year=${year}&semester=&type=&option=&data%5B%5D=`).then((semesters)=>{
@@ -129,7 +129,7 @@ router.get("semesters/:year",(req,res)=>{
         console.log("An error occurred")
     }
 })
-router.get("programmes/:year/:semester",(req,res)=>{
+router.get("/programmes/:year/:semester",(req,res)=>{
     year=req.params.year;
     semester=req.params.semester;
     try{
