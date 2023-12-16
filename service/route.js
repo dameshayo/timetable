950b9198d2329155ac09757648338756e7a897fb
dameshayo <85487775+dameshayo@users.noreply.github.com>
2023-12-16 23:34:42 +0300

    Update route.js
    final changes

--- service/route.js
+++ service/route.js
@@ -518,7 +518,7 @@ router.get(
         const domain = endIndex !== -1 ? fullEndpoint.substring(0, endIndex) : fullEndpoint;
       // console.log("domain " + domain, fullEndpoint);
       const programsResponse = await axios.get(
-        `https://expensive-shorts-fox.cyclic.app/app/api/programmes/10/3356`
+        `https://expensive-shorts-fox.cyclic.app/app/api/programmes/${year}/${semester}`
       );
       
       const programs = programsResponse.data.programmes;
@@ -536,7 +536,7 @@ router.get(
         try {
           //   await delay(1000);
           const response = await axios.get(
-            `${domain}/app/api/${type}/${programValue}/${year}/${semester}`
+            `https://expensive-shorts-fox.cyclic.app/app/api/${type}/${programValue}/${year}/${semester}`
           );
           const allSessions = [];
           response.data.data.forEach((day) => {
